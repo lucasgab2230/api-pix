385c3d164427635ea675b974af89a57a01275856
lucasgab2230 <lucas.gabriel.w2025@gmail.com>
2025-12-31 22:26:25 -0300

    fix: configure SonarCloud to use proper Jest test report format

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,15 @@ module.exports = {
   moduleNameMapper: {
     '^uuid$': require.resolve('./__mocks__/uuid.js')
   },
-  reporters: ['default', ['jest-junit', { outputDirectory: '.', outputName: 'test-report.xml' }]]
+  reporters: [
+    'default',
+    ['jest-junit', {
+      outputDirectory: '.',
+      outputName: 'test-report.xml',
+      classNameTemplate: '{classname}',
+      titleTemplate: '{title}',
+      ancestorSeparator: ' ',
+      usePathForSuiteName: true
+    }]
+  ]
 };
