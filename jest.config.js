9afac4e7e44894010adcfd095308016ea1880594
lucasgab2230 <lucas.gabriel.w2025@gmail.com>
2025-12-31 21:43:21 -0300

    ci: configure test and lint reports for SonarCloud

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,6 @@ module.exports = {
   testEnvironment: 'node',
   moduleNameMapper: {
     '^uuid$': require.resolve('./__mocks__/uuid.js')
-  }
+  },
+  reporters: ['default', ['jest-junit', { outputDirectory: '.', outputName: 'test-report.xml' }]]
 };
